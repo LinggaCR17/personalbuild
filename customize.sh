cadeb30bbd18cef7bcc6da802eb71d83e0e266ab
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2023-12-21 10:21:04 +0700

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,9 @@
+stealing="$MODPATH"
+aint="personalbuild"
+cool="$stealing/$aint"
+
+set_perm_recursive "$cool" root root 0777 0755
+set_perm_recursive "$stealing" root root 0777 0755
+
+ui_print "- $(getprop ro.boot.boot_devices)"
+ui_print "- solution for all performance"

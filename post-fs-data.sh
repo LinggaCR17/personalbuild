8d2399a7de2d4981b389136e85dd47a11ea0aaef
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2023-12-22 17:05:49 +0700

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -9,10 +9,10 @@
 	resetprop --delete init.svc.vendor.thermal-hal-1-0
 	resetprop --delete init.svc.vendor.thermal-hal-2-0
 	resetprop --delete init.svc.vendor.thermal-engine
- 	resetprop --delete init.svc.vendor.thermal.symlinks
+	resetprop --delete init.svc.vendor.thermal.symlinks
   	
 	# Xiaomi™ mi_thermald
- 	resetprop --delete init.svc.mi_thermald
+	resetprop --delete init.svc.mi_thermald
 }&
 
 # Killem

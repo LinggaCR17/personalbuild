07535bb40cf2e1ffaf5ed39bffe89fc601502f00
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2023-12-22 16:51:19 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,6 +5,6 @@ cool="$stealing/$aint"
 set_perm_recursive "$cool" root root 0777 0755
 set_perm_recursive "$stealing" root root 0777 0755
 
-set_perm_recursive "scene" root root 0777 0755
+set_perm_recursive "$stealing/scene" root root 0777 0755
 ui_print "- $(getprop ro.boot.boot_devices)"
 ui_print "- solution for all performance"

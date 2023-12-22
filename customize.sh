702aeaf0e329d1c14c1985d18a39d035ad13ca33
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2023-12-22 22:19:27 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -26,8 +26,8 @@ function FindThermal() {
         elif [[ "$systemThermal" == *"-OriFile.bck"* ]]; then
         	ui_print "- ignoring conflict file"
         else
-        system=system/vendor
-        mkdir -p $MODPATH/$system/bin; mkdir -p $MODPATH/$system/etc
+        	system=system/vendor
+        	mkdir -p $MODPATH/$system/bin; mkdir -p $MODPATH/$system/etc
             if [ $2 == "thermal" ];then
                 if [ ! -f "$3/$systemThermal-BlankFile" ]; then
                 	echo "" > "$3/$systemThermal-BlankFile"

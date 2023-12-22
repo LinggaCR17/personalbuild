eb4dd32a0e79827d9e5f135e75cb8a6806ba4a54
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2023-12-22 16:04:41 +0700

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,6 +4,9 @@
 	resetprop --delete init.svc.thermal
 	resetprop --delete init.svc.thermal_manager
 	resetprop --delete init.svc.thermalloadalgod
+	
+ 	# Other SoC support
+	resetprop --delete init.svc.vendor.thermal-hal-1-0
 }&
 
 # Always return success, even if the last write fails

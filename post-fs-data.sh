4c72df34fd45b6a73bd03c656fe12c9570997370
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2023-12-22 16:12:41 +0700

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,11 +5,14 @@
 	resetprop --delete init.svc.thermal_manager
 	resetprop --delete init.svc.thermalloadalgod
 	
- 	# Other Thermal if SoC support
+ 	# Other Thermal if SoC support it
 	resetprop --delete init.svc.vendor.thermal-hal-1-0
 	resetprop --delete init.svc.vendor.thermal-hal-2-0
 	resetprop --delete init.svc.vendor.thermal-engine
-	resetprop --delete init.svc.vendor.thermal.symlinks
+ 	resetprop --delete init.svc.vendor.thermal.symlinks
+  	
+	# Xiaomi™ mi_thermald
+ 	resetprop --delete init.svc.mi_thermald
 }&
 
 # Always return success, even if the last write fails

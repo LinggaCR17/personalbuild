2ae4683b33fcddfd201a54f0aea901ce69067dfc
Blanco ✓ <43074091+LinggaCR17@users.noreply.github.com>
2024-01-05 13:51:18 +0700

    14

--- customize.sh
+++ customize.sh
@@ -93,8 +93,8 @@ stealing="$MODPATH"
 aint="personalbuild"
 cool="$stealing/$aint"
 
-set_perm_recursive "$cool"           root root 0777 0755
-set_perm_recursive "$stealing"       root root 0777 0755
+set_perm_recursive "$cool"		   root root 0777 0755
+set_perm_recursive "$stealing"	   root root 0777 0755
 
 set_perm_recursive "$stealing/scene" root root 0777 0755
 ui_print "- Personalbuild, our big, little core"
@@ -110,40 +110,40 @@ sed -i "/description=/c description=Reboot required. personalbuild by changing y
 
 bin=xbin
 if [ ! -d system/xbin ]; then
-    bin=bin
-    mkdir $stealing/system/$bin
-    mv $stealing/system/xbin/* $stealing/system/$bin
-    rm -rf $stealing/system/xbin/*
-    rmdir $stealing/system/xbin
+	bin=bin
+	mkdir $stealing/system/$bin
+	mv $stealing/system/xbin/* $stealing/system/$bin
+	rm -rf $stealing/system/xbin/*
+	rmdir $stealing/system/xbin
 fi
 
 function FindThermal()
 {
-    for systemThermal in `ls $1 | grep $2`
-    do
-        if [[ "$systemThermal" == *"-BlankFile"* ]]; then
-        	ui_print "- pervious file → $1/$systemThermal"
-        elif [[ "$systemThermal" == *"-OriFile.bck"* ]]; then
-        	ui_print "- conflict file → $1/$systemThermal"
-        else
-        	ui_print "- found file → $1/$systemThermal"
-        	system=system/vendor
-        	mkdir -p $stealing/$system/bin; mkdir -p $stealing/$system/etc
-            if [ $2 == "thermal" ]; then
-                if [ ! -f "$3/$systemThermal-BlankFile" ]; then
-                	echo "" > "$3/$systemThermal-BlankFile"
-                fi
-                if [ ! -f "$3/$systemThermal-OriFile.bck" ]; then
-                    cp -af "$1/$systemThermal" "$3/$systemThermal-OriFile.bck"
-                fi
-                cp -af "$3/$systemThermal-BlankFile" "$3/$systemThermal"
-            else
-                if [ ! -f "$3/$systemThermal" ]; then
-                    cp -af "$1/$systemThermal" "$3/$systemThermal"
-                fi
-            fi
-        fi
-    done
+	for systemThermal in `ls $1 | grep $2`
+	do
+		if [[ "$systemThermal" == *"-BlankFile"* ]]; then
+			ui_print "- pervious file → $1/$systemThermal"
+		elif [[ "$systemThermal" == *"-OriFile.bck"* ]]; then
+			ui_print "- conflict file → $1/$systemThermal"
+		else
+			ui_print "- found file → $1/$systemThermal"
+			system=system/vendor
+			mkdir -p $stealing/$system/bin; mkdir -p $stealing/$system/etc
+			if [ $2 == "thermal" ]; then
+				if [ ! -f "$3/$systemThermal-BlankFile" ]; then
+					echo "" > "$3/$systemThermal-BlankFile"
+				fi
+				if [ ! -f "$3/$systemThermal-OriFile.bck" ]; then
+					cp -af "$1/$systemThermal" "$3/$systemThermal-OriFile.bck"
+				fi
+				cp -af "$3/$systemThermal-BlankFile" "$3/$systemThermal"
+			else
+				if [ ! -f "$3/$systemThermal" ]; then
+					cp -af "$1/$systemThermal" "$3/$systemThermal"
+				fi
+			fi
+		fi
+	done
 }
 
 FindThermal "/system/bin" '"*-OriFile.bck"' "$stealing/system/bin"
@@ -155,11 +155,11 @@ FindThermal "/vendor/etc" 'thermal' "$stealing/system/vendor/etc"
 
 echo "0" > "$stealing/system/vendor/etc/thermalStatus.info"
 
-set_perm_recursive $stealing                   0 0 0755 0777
-set_perm_recursive $stealing/system/$bin       0 0 0755 0777
+set_perm_recursive $stealing				   0 0 0755 0777
+set_perm_recursive $stealing/system/$bin	   0 0 0755 0777
 
 if [ $bin == "xbin" ]; then
-    set_perm_recursive $stealing/system/bin    0 0 0755 0777
+	set_perm_recursive $stealing/system/bin	0 0 0755 0777
 fi
 
 set_perm_recursive $stealing/system/vendor/bin 0 0 0755 0777
